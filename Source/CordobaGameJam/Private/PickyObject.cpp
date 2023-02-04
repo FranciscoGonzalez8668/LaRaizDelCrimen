b5287aca077633b7dcf5343b6c98b7c7c2f67739
Francisco González <2000908@ucc.edu.ar>
2023-02-04 01:48:22 -0300

    PickyObject attribute and getDamage

---
+++ Source/CordobaGameJam/Private/PickyObject.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PickyObject.h"
+
+// Sets default values
+APickyObject::APickyObject()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	if (isBad) {
+		realDamageMade = damageMade;
+	}
+	else {
+		realDamageMade = (-1) * healingMade;
+	}
+
+}
+
+// Called when the game starts or when spawned
+void APickyObject::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APickyObject::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/CordobaGameJam/Public/PickyObject.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PickyObject.generated.h"
+
+UCLASS(BlueprintType)
+class CORDOBAGAMEJAM_API APickyObject : public AActor
+{
+
+	GENERATED_BODY()
+	int realDamageMade = 1;			//daño real hecho, Puede ser negativo o positivo segun el tipo de enemigo que sea
+	
+public:	
+	APickyObject();
+	UPROPERTY(EditAnywhere, category = "Hostile")
+	unsigned int damageMade = 1;	//El valor publico de daño es siempre positivo ya que marca la cantidad a hacer
+	UPROPERTY(EditAnywhere, category = "Hostile")
+	unsigned int healingMade = 1;	//no puede ser negativo 
+	UPROPERTY(EditAnywhere, category = "Hostile")
+	bool isBad = false;
+
+	int getDamage(){
+		return realDamageMade;
+	}
+	// Sets default values for this actor's properties
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
